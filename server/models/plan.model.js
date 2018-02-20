f7b47f7e81bba2fec40b1912d9735fec55ce69ee
Christiana Koudigkeli <ckoudigkeli@gmail.com>
2018-02-20 11:57:41 +0200

    Plan model added

---
+++ server/models/plan.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+const bcrypt   = require('bcrypt');
+
+const PlanSchema = new Schema({
+    _creator: { type: Schema.Types.ObjectId, ref: 'Provider'},
+    image: { type: Schema.Types.ObjectId, ref: 'Image'},
+    title: { type: String, required: [true, "Title is required"] },
+    description: { type: String, required: [true, "Description is required"] },
+    price: { type: Number, required: [true, "Price is required"]},
+    startDate: { type: Date },
+    startTime: { type: TimeRanges },
+    endDate: { type: Date },    
+    endTime: { type: TimeRanges },
+    location: { type: String },
+    type: { type: String },
+    minAge: { type: Number },
+    maxAge: { type: Number },
+    sex: { type: String }
+});
+
+const Plan = mongoose.model('Plan', PlanSchema);
+module.exports = Plan;
