b67289ac4cd708c3d04d3889ea9fa05d7f580380
Christiana Koudigkeli <ckoudigkeli@gmail.com>
2018-02-20 12:11:27 +0200

    Plan model and controller added

--- server/routes/api/index.controller.js
+++ server/routes/api/index.controller.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router  = express.Router();
 
 const authRoutes = require('./authentication.controller');
+const eventRoutes = require('./plans.controller');
 
 router.use('/', authRoutes);
+router.use('/events', eventRoutes);
 
 module.exports = router;
