534a5cb9ab22321415221d35933cecdef9b940e1
Sotiris Zaires <sotiriszair@gmail.com>
2018-02-20 20:05:39 +0200

    eventpage component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { EventpageComponent } from './eventpage/eventpage.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventpageComponent
   ],
   imports: [
     BrowserModule
