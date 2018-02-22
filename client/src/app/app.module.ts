ab945852e15199bba14702ab0fb8e7e5d101d26c
Gkartzonikas Marios <gartzonikas@windowslive.com>
2018-02-22 13:54:03 +0200

    buy points page

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { SearchResultsComponent } from './search-results/search-results.component';
 import { PdfDownloaderComponent } from './pdf-downloader/pdf-downloader.component';
+import { BuypointsComponent } from './buypoints/buypoints.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { PdfDownloaderComponent } from './pdf-downloader/pdf-downloader.componen
     HeaderComponent,
     FooterComponent,
     SearchResultsComponent,
-    PdfDownloaderComponent
+    PdfDownloaderComponent,
+    BuypointsComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/buypoints/buypoints.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buypoints',
+  templateUrl: './buypoints.component.html',
+  styleUrls: ['./buypoints.component.css']
+})
+export class BuypointsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
