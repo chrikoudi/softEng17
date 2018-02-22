53d1f7b53df3fd995779f02cb9e52e006aa06951
Gkartzonikas Marios <gartzonikas@windowslive.com>
2018-02-22 14:08:54 +0200

    fixes

--- client/src/app/routes.ts
+++ client/src/app/routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginformComponent } from './loginform/loginform.component';
 import { HomeComponent } from './home/home.component';
 import { EventpageComponent } from './eventpage/eventpage.component';
+import { BuypointsComponent } from './buypoints/buypoints.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginformComponent },
     { path: 'home', component: HomeComponent },
     { path: 'events', component: EventpageComponent },
+    { path: 'buypoints', component: BuypointsComponent },
     { path: '**', redirectTo: 'home' }
 ];
